00114c454b14d4bdd00446fb07ca5c0f12276cdb
elmoknimedamin <elmokni.medamin@gmail.com>
2022-12-02 05:21:48 +0100

    added admin

--- sendPrivateMessage.js
+++ sendPrivateMessage.js
@@ -1,4 +1,4 @@
-const admins = ["314073451646287875"];
+const admins = ["314073451646287875", "1048087259796230175"];
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
 
 export default async (client, replyText) => {
