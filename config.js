4f53ee6627ed9e7c2eed49447b0b150da2810751
elmoknimedamin <elmokni.medamin@gmail.com>
2022-12-02 05:25:10 +0100

    changed api url

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ console.log(process.env.DISCORD_TOKEN);
 export const config = {
     botToken : process.env.DISCORD_TOKEN,
     appID : process.env.DISCORD_APP_ID,
-    apiUrl : process.env.API_URL //192.168.1.4
+    apiUrl : "http://192.168.1.4:60004"
 }
